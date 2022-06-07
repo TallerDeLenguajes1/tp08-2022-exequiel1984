fae1f0cce354171c743903455835b3769ef4ba9e
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-06-07 16:43:00 -0300

    creo listado y agrego archivo gitignore

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+Console.WriteLine("Ingrese el directorio:");
+string path = Console.ReadLine();
+
+List<string> ListadodeCarpetasEnDirectorio = Directory.GetFiles(path).ToList();
+List<string> archivos=new List<string>();
+
+Console.WriteLine("\nCarpetas en repositorio " + path + "\n");
+
+foreach (var archivo in ListadodeCarpetasEnDirectorio)
+{
+    Console.WriteLine(archivo);
+    archivos.Add(Path.GetFileNameWithoutExtension(archivo)+","+Path.GetExtension(archivo));
+    
+    
+}
+File.WriteAllLines("CarpetasEnDirectorio.csv", archivos);
